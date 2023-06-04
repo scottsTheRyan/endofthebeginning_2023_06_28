ba69afb7b8f7a9d97c1040bdafedcb7801ad9139
thedarkness <sthomas25854@protonmail.com>
2023-06-04 13:44:20 -0700

    reset

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
     <Wrapper className='section-center'>
       <article className='content'>
         <h1>
-          time travelinnnnnn' <br />
+          time travel <br />
           for the rest of us
         </h1>
         <p>
