26aea9df537a4cbbc58d989fd4ac954420560f60
thedarkness <sthomas25854@protonmail.com>
2023-06-04 13:31:52 -0700

    process.env

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <Auth0Provider
-    //domain={process.env.REACT_APP_AUTH_DOMAIN}
-    //clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
-    domain='dev-5wn8iir8z5cg4swn.us.auth0.com'
-    clientId='ktV0IxdZR7LSkgRr2KXkSjpH4vilJOBr'
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    // domain='dev-5wn8iir8z5cg4swn.us.auth0.com'
+    // clientId='ktV0IxdZR7LSkgRr2KXkSjpH4vilJOBr'
     authorizationParams.redirect_uri={window.location.origin}
     cacheLocation="localstorage"
   >
